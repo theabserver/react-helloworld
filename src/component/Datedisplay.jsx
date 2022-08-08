5d95e6e1ad9fe66dc29819917eb2362509eb9cb2
Shawn D'Souza <shawnrussel@gmail.com>
2022-08-08 17:17:37 +0530

    Refresh button using useState Hook

--- src/component/Datedisplay.jsx
+++ src/component/Datedisplay.jsx
@@ -1,3 +1,14 @@
 import React from "react"
-var today = new Date();
-export const Datedisplay = () => <p>The time is: {today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()}</p>
+var today = new Date()
+
+export const Datedisplay = () =>
+{
+    const getNow = () => {let today = new Date(); return today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() } // current time
+    const [now, setNow] = React.useState(getNow()) // initialisation
+    const handleClick = () => setNow(getNow())  // update time
+
+    return (
+        <p>The time is: {now} <button onClick={() => { handleClick()}}>Refresh</button></p>
+        )
+}
+{/* <p>The time is: {today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()}</p> */ }
